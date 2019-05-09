46268cf527f65bbbf99cc542e5b1ace0d5b9aa94
wuyinggui <891922381@qq.com>
2019-05-09 10:42:59 +0800

    fix class wraper

--- src/main/java/com/wyg/druidservice/proxy/MyRegistryBean.java
+++ src/main/java/com/wyg/druidservice/proxy/MyRegistryBean.java
@@ -1,6 +1,6 @@
 package com.wyg.druidservice.proxy;
 
-import com.autohome.mobile.druidservice.dao.AppInfoLogDao;
+import com.wyg.druidservice.util.ClassUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -28,17 +28,14 @@ public class MyRegistryBean implements ApplicationContextAware, BeanDefinitionRe
     }
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
-        // 需要被代理的接口
-        String interfacePackage = ctx.getEnvironment().getProperty("sql.scan.package");
-        Class[] subClasses = null;
+        ArrayList<Class<?>> subClasses = null;
         try {
-            subClasses = getClasses(interfacePackage);
+            subClasses = ClassUtil.getClassesForPackage(interfacePackage);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        if (subClasses == null || subClasses.length == 0){
+
+        if (subClasses == null || subClasses.size() == 0){
             return;
         }
         for (Class cls : subClasses) {
@@ -57,54 +54,6 @@ public class MyRegistryBean implements ApplicationContextAware, BeanDefinitionRe
         this.ctx = ctx;
     }
 
-    /**
-     * 获取包下的所有class
-     *
-     * @param packageName The base package
-     * @return The classes
-     * @throws ClassNotFoundException
-     * @throws IOException
-     */
-    private static Class[] getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        List<Class> classes = new ArrayList();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes.toArray(new Class[classes.size()]);
-    }
-
-    /**
-     * 获取目录下的所有子class
-     *
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException
-     */
-    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                continue;
-            }
-            classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-        }
-        return classes;
-    }
 
     //首字母转小写
     public static String toLowerCaseFirstOne(String s)
